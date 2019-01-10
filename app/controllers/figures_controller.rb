e87f233b68950e621ae5927849dfd0b6796e590b
Cheng Vue <cvu3@ucsc.edu>
2019-01-10 10:00:18 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,13 @@
 class FiguresController < ApplicationController
 
+  get "/figures" do 
+    @figures = Figure.all
+    erb :index
+  end
+  
   get "/figures/new" do
     erb :"figures/new"
   end
+
+  
 end
