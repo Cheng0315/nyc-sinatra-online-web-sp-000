21c8a61696fabd054f17ab220015c7444ce2afe1
Cheng Vue <cvu3@ucsc.edu>
2019-01-10 12:21:57 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,7 +11,7 @@ class FiguresController < ApplicationController
 
   post "/figures" do
     @figure = Figure.create(params[:figure])
-    if !params[:landmark][:name].empty? && !params[:landmark][:year_completed].empty?
+    if !params[:landmark][:name].empty?
       @landmark = Landmark.create(params[:landmark])
       @figure.landmarks << @landmark
     end
